9dcbb99ed20d74381e8db1534946eea93eea3c95
Max van den Bosch <maxdaniel98@gmail.com>
2017-12-06 23:41:25 +0100

    hardware permission fix

--- pilight/run.sh
+++ pilight/run.sh
@@ -6,8 +6,8 @@ DEVICE=$(jq -r '.device // empty' "$CONFIG_PATH")
 DEVICE=${GPIO_PLATFORM:-none} # default to none
 
 # Update pilight config
-sed -i 's/\("comport"\): \?".*"\(.*\)/\1: "'"$DEVICE"'"\2/' /etc/pilight/config.json
-#cat CONFIG_PATH > /etc/pilight/config.json
+#sed -i 's/\("comport"\): \?".*"\(.*\)/\1: "'"$DEVICE"'"\2/' /etc/pilight/config.json
+cat CONFIG_PATH > /etc/pilight/config.json
 
 # Run pilight in foreground
 exec pilight-daemon -D
