6e734063f203909ed639533ca6f9a1400f730d85
Max van den Bosch <maxdaniel98@gmail.com>
2017-12-06 22:34:31 +0100

    Max's edits (support for pilight USB Nano)

--- pilight/run.sh
+++ pilight/run.sh
@@ -6,7 +6,8 @@ GPIO_PLATFORM=$(jq -r '.gpio_platform // empty' "$CONFIG_PATH")
 GPIO_PLATFORM=${GPIO_PLATFORM:-none} # default to none
 
 # Update pilight config
-sed -i 's/\("gpio-platform"\): \?".*"\(.*\)/\1: "'"$GPIO_PLATFORM"'"\2/' /etc/pilight/config.json
+#sed -i 's/\("gpio-platform"\): \?".*"\(.*\)/\1: "'"$GPIO_PLATFORM"'"\2/' /etc/pilight/config.json
+cat CONFIG_PATH > /etc/pilight/config.json
 
 # Run pilight in foreground
 exec pilight-daemon -D
